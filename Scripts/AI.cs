be65275ec8b186c76c524b9b27252350b53c3dce
Axel <axel.paris69@gmail.com>
2015-08-22 10:46:59 +0200

    Started the AI script !

---
+++ Scripts/AI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AI : MonoBehaviour
+{
+	// Settings
+	[Header("General Settings")]
+	[SerializeField]
+	private float awareness;
+	[SerializeField]
+	private float reactionTime;
+	[SerializeField]
+	private float movingSpeed;
+
+	[Header("Moving Settings")]
+	[SerializeField]
+	private bool canMove;
+	[SerializeField]
+	private float patrollingSpeed;
+	[SerializeField]
+	private List<Vector3> patrollingPositions;
+
+	// Private variables
+	private Vector3 lastPlayerPosition;
+
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+	}
+}
