08cd919cee37b75d74ce4a3b25d75a8d9102ef61
Axel <axel.paris69@gmail.com>
2015-08-23 21:19:31 +0200

    Finished the Patrolling AI (with no interruption), works like a charm.

--- Scripts/PatrollingAI.cs
+++ Scripts/PatrollingAI.cs
@@ -6,19 +6,43 @@ public class PatrollingAI : AI
 {
 	[Header("Moving Settings")]
 	[SerializeField]
-	private bool canMove;
-	[SerializeField]
-	private float patrollingSpeed;
-	[SerializeField]
 	private List<Vector3> patrollingPositions;
+
+	// Private variables
+	private int nextPosition;
+	private NavMeshAgent navMeshAgent;
+	private bool timerStarted = false;
+	private float timerNewPosition;
 
 	void Start ()
 	{
-	
+		// Position 0 is the starting one
+		nextPosition = 1;
+		navMeshAgent = this.GetComponent<NavMeshAgent>();
+		navMeshAgent.acceleration = acceleration;
+		navMeshAgent.speed = movingSpeed;
+		warningText.SetActive(false);
+
+		MoveToNextPosition();
 	}
 	
 	void Update ()
 	{
-	
+		if (!timerStarted && navMeshAgent.velocity.magnitude < 1f)
+		{
+			timerStarted = true;
+			timerNewPosition = Time.realtimeSinceStartup + 2f;
+        }
+		if (timerStarted && timerNewPosition - Time.realtimeSinceStartup < 0f)
+			MoveToNextPosition();
+	}
+
+	private void MoveToNextPosition()
+	{
+		navMeshAgent.SetDestination(patrollingPositions[nextPosition]);
+		timerStarted = false;
+		nextPosition++;
+		if (nextPosition >= patrollingPositions.Count)
+			nextPosition = 0;
 	}
 }
